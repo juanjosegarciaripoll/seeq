6b642f5d4ca1afcf29d9b0b4e01aec6d79c8705d
Juanjo Garcia Ripoll <juanjose.garciaripoll@gmail.com>
2019-08-02 17:21:22 +0200

    Missing commit for the overlap and fidelity

--- seeq/control.py
+++ seeq/control.py
@@ -4,7 +4,7 @@ from numbers import Number
 import scipy.optimize
 
 def parametric_control(x0, H, ψ0, Ug, T, dH=None, check_gradient=False,
-                       steps=100, tol=1e-10, method='chebyshev',
+                       steps=100, tol=1e-10, method='chebyshev', target='fidelity',
                        debug=False, optimizer='BFGS', **kwdargs):
     """Solve the quantum control problem for a Hamiltonian H acting on
     a basis of states ψ.
@@ -17,6 +17,7 @@ def parametric_control(x0, H, ψ0, Ug, T, dH=None, check_gradient=False,
     T      - Either a time or a vector of times
     steps  - # time steps in the algorithm (if T is a number)
     tol    - Optimization tolerance
+    target - 'fidelity' or 'overlap', depending on the cost function
     method - Solution method for the time evolution
     debug  - Return cost functions
     optimizer - Method for scipy.optimize.minimize
@@ -31,37 +32,64 @@ def parametric_control(x0, H, ψ0, Ug, T, dH=None, check_gradient=False,
     else:
         times = np.array(T)
         steps = len(T)
+    if ψ0.ndim == 1:
+        ψ0 = ψ0.reshape(ψ0.size, 1)
+    N = ψ0.shape[1]
     ξT = Ug @ ψ0
     
-    def bare_cost(x, verbose=False):
+    def compute_ψT(x):
         for t, ψ in evolve(ψ0, lambda t,ψ: H(t,x,ψ), times, method=method):
             ψT = ψ
-        return -np.vdot(ξT, ψT).real
-
-    def cost_and_gradient(x, verbose=False):
+        return ψT
+    
+    def compute_ψT_and_f(x):
         Hx = lambda t, ψ: H(t, x, ψ)
         ξ = list(ξt for t, ξt in evolve(ξT, Hx, times[-1::-1], method=method))
         ξ.reverse()
-        f = np.zeros((len(x), len(times)))
+        f = np.zeros((len(x), N, len(times)))
         for (i, ((t, ψt), ξt)) in enumerate(zip(evolve(ψ0, Hx, times, method=method), ξ)):
             ψT = ψt
-            f[:,i] = np.array([np.vdot(ξt, dHi) for dHi in dH(t, x, ψt)]).imag
-        dFdx = np.array([-scipy.integrate.simps(fx, x=times) for fx in f])
-        return -np.vdot(ξT, ψT).real, dFdx
+            f[:,:,i] = np.array([np.sum(ξt.conj()*dHi, 0) for dHi in dH(t, x, ψt)]).imag
+        return ψT, f
     
+    def fidelity(x):
+        return -np.vdot(ξT, compute_ψT(x)).real/N
+
+    def fidelity_and_gradient(x):
+        ψT, f = compute_ψT_and_f(x)
+        F = -np.vdot(ξT, ψT).real/N
+        dFdx = -scipy.integrate.simps(np.sum(f, 1), x=times, axis=-1)/N
+        return F, dFdx
+    
+    def overlap(x):
+        Fn = np.sum(ξT.conj() * compute_ψT(x), 0)
+        return -np.vdot(Fn, Fn).real/N
+
+    def overlap_and_gradient(x, verbose=False):
+        ψT, f = compute_ψT_and_f(x)
+        dFndx = scipy.integrate.simps(f, x=times, axis=-1)
+        Fn = np.sum(ξT.conj()*ψT, 0)
+        O = -np.vdot(Fn, Fn).real/N
+        dOdx = -2*np.sum(Fn.conj()*dFndx, 1).real/N
+        return O, dOdx
+    
+    if target == 'fidelity':
+        fn = fidelity
+        dfn = fidelity_and_gradient
+    else:
+        fn = overlap
+        dfn = overlap_and_gradient
     if dH is None:
-        r = scipy.optimize.minimize(bare_cost, x0, tol=tol, method=optimizer)
-        fn = bare_cost
+        r = scipy.optimize.minimize(fn, x0, tol=tol, method=optimizer)
     else:
         if check_gradient:
-            _, dFdx = cost_and_gradient(x0)
-            dFdx2 = scipy.optimize.approx_fprime(x0, bare_cost, 1e-6)
-            err = np.max(np.abs(dFdx2 - dFdx))
+            _, grad = dfn(x0)
+            grad2 = scipy.optimize.approx_fprime(x0, fn, 1e-6)
+            err = np.max(np.abs(grad2 - grad))
             print(f'max gradient error:   {err}')
-            print(f'Finite diff gradient: {dFdx2}')
-            print(f'Our estimate:         {dFdx}')
-        r = scipy.optimize.minimize(cost_and_gradient, x0, tol=tol, jac=True, method=optimizer)
-        fn = cost_and_gradient
+            print(f'Finite diff gradient: {grad2}')
+            print(f'Our estimate:         {grad}')
+        r = scipy.optimize.minimize(dfn, x0, tol=tol, jac=True, method=optimizer)
     if debug:
         return r, fn
     else:
--- seeq/test/test_parametric_control.py
+++ seeq/test/test_parametric_control.py
@@ -1,5 +1,9 @@
 
-from mps.test.tools import *
+import numpy as np
+import scipy.sparse as sp
+from seeq.states import *
+
+from seeq.control import *
 
 import unittest
 
@@ -9,7 +13,7 @@ class TestQControl(unittest.TestCase):
     σx = np.array([[0., 1.],[1., 0.]])
     σy = np.array([[0., -1.j],[1.j, 0.]])
     ψ0 = np.eye(2)
-    
+
     def test_nothing(self):
         """For a qubit to remain the same, we do nothing."""
         Ug = np.eye(2)
@@ -32,5 +36,23 @@ class TestQControl(unittest.TestCase):
         Ug = -1j*self.σy
         H = lambda t, x, ψ: (x * self.σy) @ ψ
         r = parametric_control([1.0], H, self.ψ0, Ug, T=1.0, tol=1e-9, method='expm')
+        self.assertEqual(len(r.x), 1)
+        self.assertAlmostEqual(r.x[0], self.π/2., delta=1e-7)
+
+    def test_nothing_derivative(self):
+        """For a qubit to remain the same, we do nothing (with gradients)."""
+        Ug = np.eye(2)
+        H = lambda t, x, ψ: x * (self.σx @ ψ)
+        dH = lambda t, x, ψ: [self.σx @ ψ]
+        r = parametric_control([1.0], H, self.ψ0, Ug, T=1.0, dH=dH, tol=1e-8, method='expm')
+        self.assertEqual(len(r.x), 1)
+        self.assertAlmostEqual(r.x[0], 0.0, delta=1e-7)
+
+    def test_qubit_flip_derivative(self):
+        """Construct a π/2 pulse (with gradients)."""
+        Ug = -1j*self.σy
+        H = lambda t, x, ψ: (x * self.σy) @ ψ
+        dH = lambda t, x, ψ: [self.σy @ ψ]
+        r = parametric_control([1.0], H, self.ψ0, Ug, T=1.0, dH=dH, tol=1e-9, method='expm')
         self.assertEqual(len(r.x), 1)
         self.assertAlmostEqual(r.x[0], self.π/2., delta=1e-7)
