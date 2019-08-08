b471ba27e48aba7805f9731046f90c9d02a6eb35
Juanjo Garcia Ripoll <juanjose.garciaripoll@gmail.com>
2019-08-08 15:53:16 +0200

    Regular3DLattice was not exported. Output was garbled into Lattices.ipynb.

--- seeq/models/lattice.py
+++ seeq/models/lattice.py
@@ -36,6 +36,77 @@ class Lattice(LinearOperator):
         # Inherited interface from LinearOperator()
         return self._H @ A
 
+class Regular3DLattice(Lattice):
+    """Regular lattice of equispaced points, with arbitrary connectivity and boundary.
+    
+    Parameters
+    ----------
+    sizes     -- tuple (Lx, Ly, Lz) of sizes for the lattice
+    hopping   -- function of three arguments (X,Y,Z), returning a list of pairs
+                 [(J, (X,Y,Z))] with the hopping and the neighbor's coordinate.
+    r0        -- location of the corner (defaults to (0,0,0))
+    condition -- function of three arguments (X,Y,Z), returning true if the point
+                 belongs to the lattice.
+    """
+    
+    def __init__(self, sizes, hopping, g=None, r0=(0,0,0), condition=None):
+        
+        if condition == None:
+            condition = lambda X, Y, Z: True
+
+        # Construct all points in this lattice
+        X0, Y0, Z0 = r0
+        coord = [(X,Y,Z)
+                 for X in range(X0,X0+sizes[0])
+                 for Y in range(Y0,Y0+sizes[1])
+                 for Z in range(Z0,Z0+sizes[2])
+                 if condition(X, Y, Z)]
+        #
+        # Assign an index to each point
+        ndx_map = {vector: ndx for ndx, vector in enumerate(coord)}
+        #
+        # Construct the list of neighbors for each point, together
+        # with their hoppings
+        hops = np.array([(J, i, ndx_map[dest])
+                         for i, orig in enumerate(coord)
+                         for J, dest in hopping(*orig)
+                         if dest in ndx_map])
+        #
+        # Use this information to build the sparse matrix `H` of
+        # hoppings and frequencies.
+        L = len(ndx_map)
+        H = sp.csr_matrix((hops[:,0],(hops[:,1],hops[:,2])), shape=(L,L))
+        #
+        # Determine the dimensionality 
+        if sizes[2] > 1:
+            dimension = 3
+        elif sizes[1] > 1:
+            dimension = 2
+        else:
+            dimension = 1
+        super(Regular3DLattice, self).__init__(H, dimension)
+        self.coord = coord = np.array(coord)
+        self.ndx_map = ndx_map
+        self.Xmin = min(coord[:,0])
+        self.Xmax = max(coord[:,0])
+        self.Ymin = min(coord[:,1])
+        self.Ymax = max(coord[:,1])
+        self.Zmin = min(coord[:,2])
+        self.Zmax = max(coord[:,2])
+
+    def coupling_at(self, r, g=1.0):
+        """Return vector of couplings at given position."""
+        ndx = ndx_map.get(r, None)
+        if ndx is None:
+            raise Exception(f'Emitter position {r} is not in the lattice.')
+        gr = np.zeros(self.sizeL)
+        gr[ndx] = g
+        return gr
+
+    def vertex_index(self, r):
+        """Return the index of position 'r' in the lattice."""
+        return self.ndx_map[r]
+
 class Lattice1D(Regular3DLattice):
     """Latice for a 1D model with nearest-neighbor hoppings.
     
