57c7cc6a3679bf2b3a844fb0e6fb46e66016dbef
Juan Jose Garcia-Ripoll <juanjose.garciaripoll@gmail.com>
2019-10-02 21:10:27 +0200

    Added a function to plot fields on a lattice

--- seeq/models/lattice_plot.py
+++ seeq/models/lattice_plot.py
@@ -2,6 +2,8 @@
 import matplotlib.collections
 import matplotlib.pyplot as plt
 import matplotlib.tri as tri
+import scipy.sparse as sp
+import numpy as np
 
 def plot_lattice(L, ax=None, dot='.'):
     """Plot a 2D or 3D representation of the lattice on the given
@@ -52,3 +54,39 @@ def plot_lattice(L, ax=None, dot='.'):
     else:
         ax.plot(points[:,0], points[:,1], '.')
     return ax
+
+def plot_field2d(lattice, field, ax=None, x=None, y=None,
+                 Lx=100, Ly=100, σ=1/2.0, cmap='Greys'):
+    """Plot a field that lives in a 2D lattice.
+    
+    Parameters
+    ----------
+    L    -- A Regular3DLattice() object
+    ax   -- Axis to plot on. If None, create a new one.
+    dot  -- Symbol to plot on the vertices of the lattice.
+    
+    Returns
+    -------
+    ax   -- Axis on which the figure is plot.
+    """
+    if ax is None:
+        fig, ax = plt.subplots()
+    if x is None:
+        x = np.linspace(lattice.Xmin-σ, lattice.Xmax+σ, Lx)
+    else:
+        Lx = len(x)
+    if y is None:
+        y = np.linspace(lattice.Ymin-σ, lattice.Ymax+σ, Ly)
+    else:
+        Ly = len(y)
+    extent = [np.min(x), np.max(x), np.min(y), np.max(y)]
+    aspect = (extent[1]-extent[0])/(extent[3]-extent[2])/1
+    aspect = 'auto'
+    x = np.reshape(x, (1,Lx))
+    y = np.reshape(y, (Ly,1))
+    dty = np.zeros((Ly, Lx))
+    for (n, (X, Y, Z)) in zip(field, lattice.coord):
+        dty += n * np.exp(-((x-X)**2+(y-Y)**2)/σ**2)
+    ax.imshow(dty, extent=extent, aspect=aspect, interpolation='none',
+              origin='lower', cmap=cmap)
+    return ax, dty
