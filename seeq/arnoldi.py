The Arnoldi method must live on a separate file. Fix order of operations to avoid numerical instability in Arnoldi reorthogonalization.

seeq/arnoldi.py
@@ -8,7 +8,7 @@ import warnings
 class AccuracyWarning(Warning):
     pass
 
-class LanczosExpm:
+class ArnoldiExpm:
     def __init__(self, H, d=0):
         #
         # H can be a function, a matrix, a sparse matrix, etc. We construct
@@ -17,57 +17,72 @@ class LanczosExpm:
         #
         if callable(H) and not isinstance(H, sla.LinearOperator):
             H = sla.LinearOperator((d,d),matvec=H)
-        self.H = H
+        self.H = H            
+    
+    def estimateOrder(self, dt=1.0):
+        #
+        # Estimate the order of the Lanczos expansion
+        #
+        self.Hnorm = abs(sla.eigs(self.H, k=1, which='LM', return_eigenvectors=0)[0])
+        return max(int(3*self.Hnorm*dt+1),4)
 
-    def apply(self, v, dt=1.0, order=None, tol=1e-14):
+    def apply(self, v, dt=1.0, order=None, tol=1e-13):
         # This function has two ways to operate: if we provide an order,
         # it applies the Lanczos expansion up to that basis size; otherwise
         # it estimates the number of vectors based on the norm of the matrix
         # that we will exponentiate (which was estimated before in __init__)
-        nmax = 10 if order is None else order
-        if nmax > v.size:
-            nmax = v.size
+        nmax = 12 if order is None else order
         #
         # Construct a projected version of the matrix 'H' on the
         # Krylov subspace generated around vector 'v'
         #
-        v = numpy.array(v)
-        vnrm = la.norm(v)
-        vn = v / vnrm
-        vnm1 = numpy.zeros(v.shape)
-        α = []
-        β = [0.0]
+        v = numpy.asarray(v)
+        β = numpy.linalg.norm(v)
+        vn = v / β
+        V = []
+        H = scipy.sparse.dok_matrix((v.size,v.size), dtype=numpy.complex128)
+        lasterr = 0
         start = 0
-        lasterr = vnrm * 1e10
         while True:
-            #
-            # Iteratively extend the Krylov basis using the lanczos
-            # recursion without restart or reorthogonalization.
-            #
-            for n in range(start, nmax):
-                w = numpy.asarray(self.H @ vn)
-                α.append(numpy.vdot(vn, w))
-                w = w - α[n] * vn - β[n] * vnm1
-                vnm1 = vn
-                aux = la.norm(w)
-                β.append(aux)
-                if aux < 1e-20:
+            for j in range(start,nmax):
+                V.append(vn)
+                w = (-1j*dt)*(self.H @ vn)
+                for (i,Vi) in enumerate(V):
+                    H[i,j] = hij = numpy.vdot(Vi, w)
+                    w -= hij * Vi
+                H[j+1,j] = hlast = numpy.linalg.norm(w)
+                if hlast < 1e-16:
+                    # Stop if our vectors become too small
                     break
-                vn = w / aux
+                w /= hlast
+                vn = w
             #
             # We diagonalize the banded matrix formed by α and β and
             # use that to compute the exponential of the effective
             # truncated matrix. This also allows us to estimate the error
             # due to the Lanczos method.
             #
-            w, u = scipy.linalg.eig_banded(numpy.array([β[:-1],α]),
-                                           overwrite_a_band=True)
-            fHt = u @ (numpy.exp(-1j*dt*w) * u[0,:].conj())
-            err = abs(fHt[n]*β[n+1])
+            Hj = H.copy()
+            if True:
+                Hj.resize((j+2,j+2))
+                Hj[j+1,j+1] = 1.0
+                e1 = np.zeros(j+2)
+                e1[0] = 1.0
+                y = scipy.sparse.linalg.expm_multiply(Hj.tocsr(), e1)
+                #y = scipy.linalg.expm(Hj.todense())[:,0]
+                err = abs(hlast * y[-1])
+                y = y[:-1]
+            else:
+                Hj.resize((j+1,j+1))
+                e1 = np.zeros(j+1)
+                e1[0] = 1.0
+                y = scipy.sparse.linalg.expm_multiply(Hj.tocsr(), e1)
+                #y = scipy.linalg.expm(Hj.todense())[:,0]
+                err = abs(hlast * y[-1])
             if err < tol:
                 break
-            if lasterr < err or nmax == v.size or (order is not None):
-                warnings.warn(f'Lanczos failed to converge at {len(α)} iterations with error {err}',
+            if (nmax == v.size) or (order is not None):
+                warnings.warn(f'Arnoldi failed to converge at {j+1} iterations with error {err}',
                               AccuracyWarning)
                 lasterr = err
                 break
@@ -77,17 +92,10 @@ class LanczosExpm:
         # Given the approximation of the exponential, recompute the
         # Lanczos basis 
         #
-        vnm1 = vn = v
-        output = fHt[0] * vn
-        for n in range(1, nmax):
-            w = numpy.asarray(self.H @ vn) - α[n-1] * vn - β[n-1] * vnm1
-            vnm1 = vn
-            vn = w / β[n]
-            output = output + fHt[n] * vn
-        return output
+        return np.array(V).T @ (β * y)
     
 def expm(A, v, **kwargs):
-    """Apply the Lanczos approximation of the exponential exp(1i*dt*A)
+    """Apply the Arnoldi approximation of the exponential exp(1i*dt*A)
     onto the vector or matrix `v`.
     
     Parameters
@@ -105,6 +113,6 @@ def expm(A, v, **kwargs):
     -------
     newv      -- A vector or a matrix approximating expm(1j*dt*A) @ v
     """
-    aux = LanczosExpm(A, d=v.size)
+    aux = ArnoldiExpm(A, d=v.size)
     return aux.apply(v, **kwargs)
 

seeq/chebyshev.py
@@ -60,7 +60,7 @@ class ChebyshevExpm:
         ndx = np.arange(0,order)
         return jn(ndx, rm) * ((-1j)**ndx)
                     
-    def apply(self, v, order=100, dt=1.0, tol=1e-14):
+    def apply(self, v, dt=1.0, order=None, maxorder=None, tol=1e-14):
         """Apply the Chebyshev approximation of the exponential exp(1i*dt*A)
         onto the vector or matrix `v`.        
         Parameters
@@ -73,7 +73,8 @@ class ChebyshevExpm:
         """
         rp = dt * self.center
         rm = dt * self.height
-        order = max(order, 2 * int(rm))
+        if order is None:
+            order = max(100, 2*int(rm))
         
         # Apply a version of A that is shifted and rescaled 
         def Btimes(phi):